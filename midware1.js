63a37d04b1a2aac2ca36b49dc210d2fe70d0a287
nishthasingh20 <singhnishtha206@gmail.com>
2025-03-27 11:20:48 +0530

    working

--- midware1.js
+++ midware1.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 
 app.get("/health-checkup", function(req, res) {
-    const username = req.headers.username;
-    const password = req.headers.password;
-    const kidneyId = req.query.kidneyId;
+    const username = req.headers.username;  //in header
+    const password = req.headers.password;  //in header
+    const kidneyId = req.query.kidneyId;  //as a query parameter
 
     if(!(username === "nishtha" && password === "pass")) { //same as username != "nishtha" || password != "pass"  
-        res.status(400).json({"msg": "Invalid inputs"})
+        res.status(400).json({
+            "msg": "Invalid inputs"
+        })
         return 
     }
 
