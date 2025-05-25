efc735f6cbf77f9dbc43a6cbdc2d0dfa84b9ccdd
nishthasingh20 <singhnishtha206@gmail.com>
2025-05-25 22:52:21 +0530

    sample data

--- index.js
+++ index.js
@@ -3,19 +3,31 @@ mongoose.connect("mongodb+srv://singhnishtha206:nish_mongo20S@cluster0.76iht.mon
 
 const User = mongoose.model('users', { name: String, email: String, password: String });
 
-const user = new User({ 
-    name: 'Nishtha', 
-    email: 'abcd@example.com', 
-    password: '123456'
-});
+app.post("/signup", async function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
 
-async function saveUser() {
+    const existingUser = await User.findOne({ email: username });  //1:41
+    if (existingUser) {
+        return res.status(400).send("Username already exists");
+    }
+
+    const user = new User({ 
+        name: 'Nishtha', 
+        email: 'abcd@example.com', 
+        password: '123456'
+    });
+
     try {
         const savedUser = await user.save();
         console.log('User saved successfully:', savedUser);
     } catch (err) {
         console.error('Error saving user:', err);
+        return res.status(500).send("Error saving user");
     }
-}
 
-saveUser();
+    res.json({
+        "msg": "User created successfully"
+    })
+});
